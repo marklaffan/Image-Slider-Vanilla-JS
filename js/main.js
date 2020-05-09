fd359d0f1910577e3ad992bec7f069bd98670c42
Mark Laffan <marklaffan4@gmail.com>
2020-05-09 09:38:53 +0100

    Built a carousel version from a website tutorial

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
 // JavaScript Document
+
+
+var slideInterval = 4000;
+
+function getFigures() {
+	return document.getElementById('carousel').getElementsByTagName('figure');
+} // End of Function
+
+
+function moveForward() {
+	var pointer = 0;
+	var figures = getFigures();
+	for (var i=0; i<figures.length; i++) {
+		if (figures[i].className == 'visible') {
+			figures[i].className = 'hidden';
+			pointer = i;
+		}  // End of If statement
+	}  // End of For loop
+	if (++pointer == figures.length) {
+		pointer = 0;
+	}  // End of If statement
+	figures[pointer].className = 'visible';
+	setTimeout(moveForward, slideInterval);
+}  // End of Function
+
+
+function startPlayback() {
+	setTimeout(moveForward, slideInterval);
+} // End of Function
+
+
+startPlayback();
